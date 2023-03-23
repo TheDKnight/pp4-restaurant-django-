952379fa14fe38a791b54b9f6396b80b79a3881e
TheDKnight <113733655@umail.ucc.ie>
2023-03-23 23:21:10 +0000

    add

--- sun_of_a_bun/settings.py
+++ sun_of_a_bun/settings.py
@@ -82,8 +82,8 @@ MESSAGE_TAGS = {
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
